be78f43c876b16314a584203f845fc8dfe0d5e59
Fede (UCM) <federami@ucm.es>
2024-12-12 11:58:11 +0100

    Changes when reading the sourcedata of LEMON_database

--- scripts/miscellaneous/divide_resting_LEMON_database.py
+++ scripts/miscellaneous/divide_resting_LEMON_database.py
@@ -21,74 +21,72 @@ config['rs_EC_code'] = 'Stimulus/S210'
 
 # Filter the subjects of interest
 config['database'] = 'LEMON_database'
-config['path'] = {'sub': os.path.join('databases','LEMON_database')}
-sub = glob.glob(os.path.join(config['path']['sub'],'sub-*37'))
-sub = [os.path.basename(current_sub) for current_sub in sub]
-
-# Go through each sub
-for current_sub in sub:
-
-    # Find the files
-    files = glob.glob(os.path.join(config['path']['sub'],current_sub,'RSEEG','*vhdr'))
-
-    for current_file in files:
-
-        print('Working on ' + current_file)
-
-        try:
-
-            # Read the file
-            raw = mne.io.read_raw(current_file,preload=True)
-
-            # Get the annotations
-            annotations = raw.annotations
-
-            #######
-            # EO
-            #######
-            # Read the annotations and go one by one
-            events, event_dict = mne.events_from_annotations(raw,regexp=config['rs_EO_code'])
-
-            # Get the epochs
-            epochs = mne.Epochs(
-                raw,
-                events=events,
-                tmin=0,tmax=2,
-                reject_by_annotation=False,
-                baseline=None,
-                preload=True)
-
-            # Export
-            filename = current_sub + '_ses-1' + '_task-EO_eeg.set'
-            out_filepath = os.path.join(config['path']['sub'],'sourcedata',filename)
-            export_mne_epochs(epochs,out_filepath)
-
-            # Read the file
-            raw = mne.io.read_raw(current_file,preload=True)
-
-            # Get the annotations
-            annotations = raw.annotations
-
-            #######
-            # EC
-            #######
-            # Read the annotations and go one by one
-            events,event_dict = mne.events_from_annotations(raw,regexp=config['rs_EC_code'])
-
-            # Get the epochs
-            epochs = mne.Epochs(
-                raw,
-                events=events,
-                tmin=0,tmax=2,
-                reject_by_annotation=False,
-                baseline=None,
-                preload=True)
-
-            # Export
-            filename = current_sub + '_ses-1' + '_task-EC_eeg.set'
-            out_filepath = os.path.join(config['path']['sub'],'sourcedata',filename)
-            export_mne_epochs(epochs,out_filepath)
+config['path'] = {'sourcedata': os.path.join('databases','LEMON_database','sourcedata')}
+files = glob.glob(os.path.join(config['path']['sourcedata'],'sub*vhdr'))
+files = [os.path.basename(current_files) for current_files in files]
 
 
-        except:
-            print('   ERROR')
+for current_file in files:
+
+    print('Working on ' + current_file)
+
+    try:
+
+        #######
+        # EO
+        #######
+        # Read the file
+        #dummy_current_file = os.path.join(config['path']['sourcedata'],current_file)
+        #raw = mne.io.read_raw(dummy_current_file,preload=True)
+
+        # Get the annotations
+        #annotations = raw.annotations
+
+        # Read the annotations and go one by one
+        #events, event_dict = mne.events_from_annotations(raw,regexp=config['rs_EO_code'])
+
+        # Get the epochs
+        #epochs = mne.Epochs(
+        #    raw,
+        #    events=events,
+        #    tmin=0,tmax=2,
+        #    reject_by_annotation=False,
+        #    baseline=None,
+        #    preload=True)
+
+        # Export
+        #filename = current_file[:10] + '_ses-1' + '_task-EO_eeg.set'
+        #out_filepath = os.path.join(config['path']['sourcedata'],filename)
+        #export_mne_epochs(epochs,out_filepath)
+
+        #######
+        # EC
+        #######
+        # Read the file
+        dummy_current_file = os.path.join(config['path']['sourcedata'],current_file)
+        raw = mne.io.read_raw(dummy_current_file,preload=True)
+
+        # Get the annotations
+        annotations = raw.annotations
+
+
+        # Read the annotations and go one by one
+        events,event_dict = mne.events_from_annotations(raw,regexp=config['rs_EC_code'])
+
+        # Get the epochs
+        epochs = mne.Epochs(
+            raw,
+            events=events,
+            tmin=0,tmax=2,
+            reject_by_annotation=False,
+            baseline=None,
+            preload=True)
+
+        # Export
+        filename = current_file[:10] + '_ses-1' + '_task-EC_eeg.set'
+        out_filepath = os.path.join(config['path']['sourcedata'],filename)
+        export_mne_epochs(epochs,out_filepath)
+
+
+    except:
+        print('   ERROR')
--- scripts/s0a_run_ETL_on_databases/run_ETL.py
+++ scripts/s0a_run_ETL_on_databases/run_ETL.py
@@ -21,7 +21,7 @@ from sEEGnal.io.export_clean import export_clean
 database = 'LEMON_database'
 
 # What step to run: standardize, badchannel, artifact, export_clean
-run = [1,0,0,0]
+run = [1,1,1,1]
 
 # Init the database
 config, files, sub, ses, task = init_database(database)
@@ -31,6 +31,9 @@ errors = []
 
 # Go through each subject
 for current_index in range(len(files)):
+
+        if current_index < 20:
+            continue
 
         # current info
         current_file = files[current_index]
