ca0cc95741457b8af8b0b2a30567b3d6885b4130
Fede_C3N <fede.ramirez.t@gmail.com>
2025-05-29 15:58:36 +0200

    Create test_retest database

--- sEEGnal/io/bids.py
+++ sEEGnal/io/bids.py
@@ -666,3 +666,22 @@ def create_LEMON_database(config,current_file,current_sub,current_ses,current_ta
 
     return bids_path
 
+
+
+def create_test_retest_database(config,current_file,current_sub,current_ses,current_task):
+
+    # Remove the unallowed characters
+    current_sub = current_sub.replace('-','')
+    current_task = current_task.replace('-','')
+
+    # Builds the BIDS path from the metadata.
+    bids_path = mne_bids.BIDSPath(
+        subject=current_sub,
+        session=current_ses,
+        task=current_task,
+        datatype='eeg',
+        root=config['path']['data_root'],
+        suffix='eeg',
+        extension='.vhdr')
+
+    return bids_path
--- sEEGnal/io/init_databases.py
+++ sEEGnal/io/init_databases.py
@@ -151,3 +151,68 @@ def init_LEMON_database(config):
     task = [match[0][2] for match in matches]
 
     return config, files, sub, ses, task
+
+
+
+def init_test_retest_database(config):
+
+    # Folders to find the subjects
+    config['path'] = {}
+    config['path']['data_root'] = os.path.join('databases','test_retest_database')
+    config['path']['sourcedata'] = os.path.join(config['path']['data_root'],'sourcedata')
+
+    # Add extra info to the dictionary
+    config['component_estimation']["channels_to_include"] = [
+        'Fp1', 'Fpz', 'Fp2', 'F7', 'F3', 'Fz', 'F4', 'F8', 'FC5', 'FC1', 'FC2', 'FC6',
+        'M1', 'T7', 'C3', 'Cz', 'C4', 'T8', 'M2', 'CP5', 'CP1', 'CP2', 'CP6', 'P7', 'P3',
+        'Pz', 'P4', 'P8', 'POz', 'O1', 'O2', 'AF7', 'AF3', 'AF4', 'AF8', 'F5', 'F1', 'F2',
+        'F6', 'FC3', 'FCz', 'FC4', 'C5', 'C1', 'C2', 'C6', 'CP3', 'CP4', 'P5', 'P1', 'P2',
+        'P6', 'F9', 'PO3', 'PO4', 'F10', 'FT7', 'FT8', 'TP7', 'TP8', 'PO7', 'PO8', 'FT9',
+        'FT10', 'TPP9h', 'TPP10h', 'PO9', 'PO10', 'P9', 'P10', 'AFF1', 'AFz', 'AFF2', 'FFC5h',
+        'FFC3h', 'FFC4h', 'FFC6h', 'FCC5h', 'FCC3h', 'FCC4h', 'FCC6h', 'CCP5h', 'CCP3h', 'CCP4h',
+        'CCP6h', 'CPP5h', 'CPP3h', 'CPP4h', 'CPP6h', 'PPO1', 'PPO2', 'I1', 'Iz', 'I2', 'AFp3h', 'AFp4h',
+        'AFF5h', 'AFF6h', 'FFT7h', 'FFC1h', 'FFC2h', 'FFT8h', 'FTT9h', 'FTT7h', 'FCC1h', 'FCC2h', 'FTT8h',
+        'FTT10h', 'TTP7h', 'CCP1h', 'CCP2h', 'TTP8h', 'TPP7h', 'CPP1h', 'CPP2h', 'TPP8h', 'PPO9h', 'PPO5h',
+        'PPO6h', 'PPO10h', 'POO9h', 'POO3h', 'POO4h', 'POO10h', 'OI1h', 'OI2h']
+    config['component_estimation']["channels_to_exclude"] = ['EMG1', 'EMG2', 'CLAV', 'VEOGL']
+    config['badchannel_detection']["channels_to_include"] = [
+        'Fp1', 'Fpz', 'Fp2', 'F7', 'F3', 'Fz', 'F4', 'F8', 'FC5', 'FC1', 'FC2', 'FC6',
+        'M1', 'T7', 'C3', 'Cz', 'C4', 'T8', 'M2', 'CP5', 'CP1', 'CP2', 'CP6', 'P7', 'P3',
+        'Pz', 'P4', 'P8', 'POz', 'O1', 'O2', 'AF7', 'AF3', 'AF4', 'AF8', 'F5', 'F1', 'F2',
+        'F6', 'FC3', 'FCz', 'FC4', 'C5', 'C1', 'C2', 'C6', 'CP3', 'CP4', 'P5', 'P1', 'P2',
+        'P6', 'F9', 'PO3', 'PO4', 'F10', 'FT7', 'FT8', 'TP7', 'TP8', 'PO7', 'PO8', 'FT9',
+        'FT10', 'TPP9h', 'TPP10h', 'PO9', 'PO10', 'P9', 'P10', 'AFF1', 'AFz', 'AFF2', 'FFC5h',
+        'FFC3h', 'FFC4h', 'FFC6h', 'FCC5h', 'FCC3h', 'FCC4h', 'FCC6h', 'CCP5h', 'CCP3h', 'CCP4h',
+        'CCP6h', 'CPP5h', 'CPP3h', 'CPP4h', 'CPP6h', 'PPO1', 'PPO2', 'I1', 'Iz', 'I2', 'AFp3h', 'AFp4h',
+        'AFF5h', 'AFF6h', 'FFT7h', 'FFC1h', 'FFC2h', 'FFT8h', 'FTT9h', 'FTT7h', 'FCC1h', 'FCC2h', 'FTT8h',
+        'FTT10h', 'TTP7h', 'CCP1h', 'CCP2h', 'TTP8h', 'TPP7h', 'CPP1h', 'CPP2h', 'TPP8h', 'PPO9h', 'PPO5h',
+        'PPO6h', 'PPO10h', 'POO9h', 'POO3h', 'POO4h', 'POO10h', 'OI1h', 'OI2h']
+    config['badchannel_detection']["channels_to_exclude"] = ['EMG1', 'EMG2', 'CLAV', 'VEOGL']
+    config['artifact_detection']["channels_to_include"] = [
+        'Fp1', 'Fpz', 'Fp2', 'F7', 'F3', 'Fz', 'F4', 'F8', 'FC5', 'FC1', 'FC2', 'FC6',
+        'M1', 'T7', 'C3', 'Cz', 'C4', 'T8', 'M2', 'CP5', 'CP1', 'CP2', 'CP6', 'P7', 'P3',
+        'Pz', 'P4', 'P8', 'POz', 'O1', 'O2', 'AF7', 'AF3', 'AF4', 'AF8', 'F5', 'F1', 'F2',
+        'F6', 'FC3', 'FCz', 'FC4', 'C5', 'C1', 'C2', 'C6', 'CP3', 'CP4', 'P5', 'P1', 'P2',
+        'P6', 'F9', 'PO3', 'PO4', 'F10', 'FT7', 'FT8', 'TP7', 'TP8', 'PO7', 'PO8', 'FT9',
+        'FT10', 'TPP9h', 'TPP10h', 'PO9', 'PO10', 'P9', 'P10', 'AFF1', 'AFz', 'AFF2', 'FFC5h',
+        'FFC3h', 'FFC4h', 'FFC6h', 'FCC5h', 'FCC3h', 'FCC4h', 'FCC6h', 'CCP5h', 'CCP3h', 'CCP4h',
+        'CCP6h', 'CPP5h', 'CPP3h', 'CPP4h', 'CPP6h', 'PPO1', 'PPO2', 'I1', 'Iz', 'I2', 'AFp3h', 'AFp4h',
+        'AFF5h', 'AFF6h', 'FFT7h', 'FFC1h', 'FFC2h', 'FFT8h', 'FTT9h', 'FTT7h', 'FCC1h', 'FCC2h', 'FTT8h',
+        'FTT10h', 'TTP7h', 'CCP1h', 'CCP2h', 'TTP8h', 'TPP7h', 'CPP1h', 'CPP2h', 'TPP8h', 'PPO9h', 'PPO5h',
+        'PPO6h', 'PPO10h', 'POO9h', 'POO3h', 'POO4h', 'POO10h', 'OI1h', 'OI2h']
+    config['artifact_detection']["channels_to_exclude"] = ['EMG1', 'EMG2', 'CLAV', 'VEOGL']
+    config['artifact_detection']["frontal_channels"] = ['Fp1','Fpz','Fp2','AF7','AF8']
+
+    # Filter the subjects of interest
+    files = glob.glob(os.path.join(config['path']['sourcedata'],'*.cnt'))
+    files = [os.path.basename(file) for file in files]
+
+    # Extract the info
+    pattern = "([0-9]-[0-9]{3})-([0-9])-[A-Z]_(1-EO|2-EC|3-EO|4-EC)_.*"
+    matches = [re.findall(pattern,file) for file in files]
+    matches,files = zip(*[(match,file) for match,file in zip(matches,files) if match])
+    sub = [match[0][0] for match in matches]
+    ses = [match[0][1] for match in matches]
+    task = [match[0][2] for match in matches]
+
+    return config, files, sub, ses, task
